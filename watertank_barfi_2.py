9db2701d36760dd2fe15845ff9eea0fd47ce2d60
martinoms <149338835+martinoms@users.noreply.github.com>
2025-05-31 20:36:06 +0200

    Update watertank_barfi_2.py

--- watertank_barfi_2.py
+++ watertank_barfi_2.py
@@ -311,8 +311,11 @@ elif page == "Launch Simulation":
                         cols[1].metric("Outlet Temp", f"{hx['T_out_HE']:.1f} °C")
                         cols[0].metric("Flow Rate", f"{hx['m_dot_secondary']:.2f} kg/s")
                         cols[1].metric("Power", f"{hx['power_kW']:.2f} kW")
-                        st.progress(abs(min(1.0, hx['efficiency'])), 
-                                   f"Efficiency: {hx['efficiency']*100:.1f}%")
+                        #if 0<hx['efficiency']<1:
+                        st.progress(min(1.0, hx['efficiency']), 
+                                f"Efficiency: {abs(hx['efficiency'])*100:.1f}%")
+                        
+                            
             else:
                 st.info("No heat exchanger configured")
 
